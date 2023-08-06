0a8f44de0874570933dce04d21ccde631a569081
sanket9191 <diwatesanket@gmail.com>
2023-08-06 13:59:48 +0530

    First commit
    Todos List webapp

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <p>
+        Welcome to our efficient and intuitive todo list app! Stay organized and focused with our streamlined features. Easily create, prioritize, and track tasks, set reminders, and collaborate with others. Designed for busy professionals, students, and anyone who values productivity. Say goodbye to scattered tasks and hello to productivity with our app!
+        </p>
+    </div>
+  )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Footer = () => {
+  // let footerStyle = {
+  //   position: "absolute",
+  //   top:"100vh",
+  //   width:"100%",
+
+  // }
+  return (
+    <footer className="bg-dark text-light py-3" >
+            <p className="text-center">
+            Copyright &copy; Sanket Diwate, 2023
+            </p>
+    </footer>
+  )
+}
